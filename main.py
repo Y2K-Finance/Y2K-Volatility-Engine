532d8267ee314f7d75a1c606335f2cffaa5e1b0c
Knot Here <knotquant@protonmail.com>
2024-01-25 18:08:21 +1100

    updates

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 from src.chainlink.format import csv_format
 from src.utils.volatility import calculate_realized_volatility
 # ASYNCHRONOUS DATA COLLECTION
-# from src.chainlink.update_dataset import  check_for_duplicates, sync_data_set
+from src.chainlink.update_dataset import  check_for_duplicates, sync_data_set
 # SYNCHRONOUS DATA COLLECTION
-from src.chainlink.update_sync import sync_data_set, check_for_duplicates
+# from src.chainlink.update_sync import sync_data_set, check_for_duplicates
 
 TICKER = 'eth'
-TIMESTAMP = 1706029499
+TIMESTAMP = 1705977000
 
 sync_data_set(TICKER)
 check_for_duplicates(TICKER)
