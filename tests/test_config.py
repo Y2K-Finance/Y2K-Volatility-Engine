c4a2858c0dc36282d4064cce3fb84d8f419b5315
Knot Here <knotquant@protonmail.com>
2024-01-27 03:35:23 +1100

    action testing

--- tests/test_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# test if config/api_map.json is there
-def test_rpc():
-    assert os.path.isfile('config/rpc.json') == True
-
-def test_oracle_addresses():
-    assert os.path.isfile('config/oracles.json') == True
